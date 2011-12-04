9c756020af8532d2c99ec4ba4bc64935ba135c7c
Sean Gordon <sean_aldon@hotmail.co.uk>
2011-12-04 05:21:51 +0000

    Implemented API Tweaked API slightly

--- src/com/mcbans/firestar/mcbans/MCBansAPI.java
+++ src/com/mcbans/firestar/mcbans/MCBansAPI.java
@@ -1,5 +1,9 @@
 package com.mcbans.firestar.mcbans;
 
+import com.mcbans.firestar.mcbans.pluginInterface.Ban;
+import com.mcbans.firestar.mcbans.pluginInterface.Kick;
+import org.bukkit.entity.Player;
+
 /**
  *
  * @author Sean (rakiru)
@@ -7,21 +11,51 @@ package com.mcbans.firestar.mcbans;
 public class MCBansAPI {
 
 	BukkitInterface plugin;
+	Settings config;
 
 	public MCBansAPI (BukkitInterface plugin) {
 		this.plugin = plugin;
+		this.config = plugin.Settings;
 	}
 
 	/**
 	 * Ban a player using the MCBans system
 	 *
 	 * @param playerName Full name of the player to be banned (no partial names)
+	 * @param playerIP String representation of the player's IP address
+	 * @param type Type of ban: "g" = global, "l" = local
+	 * @param reason Reason for the ban
+	 * @param adminName Name to be attached as player who placed the ban
+	 * @param duration Number of time units to temporarily ban player for
+	 * @param measure Type of time units to temporarily ban for
+	 */
+	private void banPlayer(String playerName, String playerIP, String type, String reason, String adminName, String duration, String measure) {
+		new Ban(plugin, type, playerName, playerIP, adminName, reason, duration, measure).start();
+	}
+
+	/**
+	 * Ban a player using the MCBans system
+	 *
+	 * @param playerName Full name of the player to be banned (no partial names)
+	 * @param playerIP String representation of the player's IP address
 	 * @param type Type of ban: "g" = global, "l" = local
 	 * @param reason Reason for the ban
 	 * @param adminName Name to be attached as player who placed the ban
 	 */
-	private void banPlayer(String playerName, String type, String reason, String adminName) {
-		//Implement ban here
+	private void banPlayer(String playerName, String playerIP, String type, String reason, String adminName) {
+		banPlayer(playerName, playerIP, type, reason, adminName, "", "");
+	}
+
+	/**
+	 * Ban a player globally using the MCBans system
+	 *
+	 * @param playerName Full name of the player to be banned (no partial names)
+	 * @param playerIP String representation of the player's IP address
+	 * @param reason Reason for the ban
+	 * @param adminName Name to be attached as player who placed the ban
+	 */
+	public void globalBanPlayer(String playerName, String playerIP, String reason, String adminName) {
+		banPlayer(playerName, playerIP, "globalBan", reason, adminName);
 	}
 
 	/**
@@ -32,17 +66,29 @@ public class MCBansAPI {
 	 * @param adminName Name to be attached as player who placed the ban
 	 */
 	public void globalBanPlayer(String playerName, String reason, String adminName) {
-		banPlayer(playerName, "g", reason, adminName);
+		globalBanPlayer(playerName, getPlayerIP(playerName), reason, adminName);
 	}
 
 	/**
 	 * Ban a player globally using the MCBans system as console
 	 *
 	 * @param playerName Full name of the player to be banned (no partial names)
-	 * @param adminName Name to be attached as player who placed the ban
+	 * @param reason Reason for the ban
 	 */
 	public void globalBanPlayer(String playerName, String reason) {
 		globalBanPlayer(playerName, reason, "console");
+	}
+
+	/**
+	 * Ban a player locally using the MCBans system
+	 *
+	 * @param playerName Full name of the player to be banned (no partial names)
+	 * @param playerIP String representation of the player's IP address
+	 * @param reason Reason for the ban
+	 * @param adminName Name to be attached as player who placed the ban
+	 */
+	public void localBanPlayer(String playerName, String playerIP, String reason, String adminName) {
+		banPlayer(playerName, playerIP, "localBan", reason, adminName);
 	}
 
 	/**
@@ -53,17 +99,17 @@ public class MCBansAPI {
 	 * @param adminName Name to be attached as player who placed the ban
 	 */
 	public void localBanPlayer(String playerName, String reason, String adminName) {
-		banPlayer(playerName, "l", reason, adminName);
+		localBanPlayer(playerName, getPlayerIP(playerName), reason, adminName);
 	}
 
 	/**
-	 * Ban a player locally using the MCBans system for the default ban reason
+	 * Ban a player locally using the MCBans system as console
 	 *
 	 * @param playerName Full name of the player to be banned (no partial names)
-	 * @param adminName Name to be attached as player who placed the ban
+	 * @param reason Reason for the ban
 	 */
-	public void localBanPlayer(String playerName, String adminName) {
-		localBanPlayer(playerName, plugin.Settings.getString("defaultLocal"), adminName);
+	public void localBanPlayer(String playerName, String reason) {
+		localBanPlayer(playerName, reason, "console");
 	}
 
 	/**
@@ -72,7 +118,21 @@ public class MCBansAPI {
 	 * @param playerName Full name of the player to be banned (no partial names)
 	 */
 	public void localBanPlayer(String playerName) {
-		localBanPlayer(playerName, "console");
+		localBanPlayer(playerName, config.getString("defaultLocal"));
+	}
+
+	/**
+	 * Ban a player for a specific time using the MCBans system
+	 *
+	 * @param playerName Full name of the player to be banned (no partial names)
+	 * @param playerIP String representation of the player's IP address
+	 * @param time Number of units the ban should last
+	 * @param units Units of time to measure in: "m" = minutes, "h" = hours, "d" = days
+	 * @param reason Reason for the ban
+	 * @param adminName Name to be attached as player who placed the ban
+	 */
+	public void tempBanPlayer(String playerName, String playerIP, float time, String units, String reason, String adminName) {
+		banPlayer(playerName, playerIP, "tempBan", reason, adminName);
 	}
 
 	/**
@@ -85,7 +145,20 @@ public class MCBansAPI {
 	 * @param adminName Name to be attached as player who placed the ban
 	 */
 	public void tempBanPlayer(String playerName, float time, String units, String reason, String adminName) {
-		//Implement tempban here
+		tempBanPlayer(playerName, getPlayerIP(playerName), time, units, reason, adminName);
+	}
+
+	/**
+	 * Ban a player for a specific time using the MCBans system
+	 *
+	 * @param playerName Full name of the player to be banned (no partial names)
+	 * @param playerIP String representation of the player's IP address
+	 * @param time Number of units the ban should last
+	 * @param units Units of time to measure in: "m" = minutes, "h" = hours, "d" = days
+	 * @param adminName Name to be attached as player who placed the ban
+	 */
+	public void tempBanPlayer(String playerName, String playerIP, float time, String units, String adminName) {
+		tempBanPlayer(playerName, playerIP, time, units, config.getString("defaultTemp"), adminName);
 	}
 
 	/**
@@ -97,7 +170,7 @@ public class MCBansAPI {
 	 * @param adminName Name to be attached as player who placed the ban
 	 */
 	public void tempBanPlayer(String playerName, float time, String units, String adminName) {
-		tempBanPlayer(playerName, time, units, plugin.Settings.getString("defaultTemp"), adminName);
+		tempBanPlayer(playerName, time, units, config.getString("defaultTemp"), adminName);
 	}
 
 	/**
@@ -107,7 +180,16 @@ public class MCBansAPI {
 	 * @param adminName Name to be attached as player who placed the ban
 	 */
 	public void unbanPlayer(String playerName, String adminName) {
-		//Implement unban here
+		banPlayer(playerName, "", "unBan", "", adminName);
+	}
+
+	/**
+	 * Unban a player using the MCBans system as console
+	 *
+	 * @param playerName Full name of the player to be banned (no partial names)
+	 */
+	public void unbanPlayer(String playerName) {
+		unbanPlayer(playerName, "console");
 	}
 
 	/**
@@ -118,7 +200,7 @@ public class MCBansAPI {
 	 * @param adminName Name to be attached as player who kicked the player
 	 */
 	public void kickPlayer(String playerName, String reason, String adminName) {
-		//Implement kick here
+		new Kick(config, plugin, playerName, adminName, reason).start();
 	}
 
 	/**
@@ -128,16 +210,24 @@ public class MCBansAPI {
 	 * @param adminName Name to be attached as player who kicked the player
 	 */
 	public void kickPlayer(String playerName, String adminName) {
-		kickPlayer(playerName, plugin.Settings.getString("defaultKick"), adminName);
+		kickPlayer(playerName, config.getString("defaultKick"), adminName);
 	}
 
 	/**
 	 * Kick a player using the MCBans system for the default kick reason  as console(the MCBans plugin will log the kick)
 	 *
 	 * @param playerName Full name of the player to be banned (no partial names)
-	 * @param adminName Name to be attached as player who kicked the player
 	 */
 	public void kickPlayer(String playerName) {
 		kickPlayer(playerName, "console");
+	}
+
+	private String getPlayerIP(String playerName) {
+		Player player = plugin.getServer().getPlayerExact(playerName);
+		if (player != null) {
+			return player.getAddress().getAddress().getHostAddress();
+		} else {
+			return "";
+		}
 	}
 }
